a3bafce96c319a3082d76dbba6141aceea3a6888
Jordan Cassem <cassem.codes@gmail.com>
2017-06-09 10:59:49 +0100

    Add edit funcitonality

--- app/controllers/AssetController.java
+++ app/controllers/AssetController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Transaction;
 import models.Asset;
+import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -26,5 +29,44 @@ public class AssetController extends Controller
 	public Result list(int page, String sortBy, String order, String filter)
 	{
 		return ok(views.html.list.render(Asset.page(page, 20, sortBy, order, filter), sortBy, order, filter));
+	}
+
+	public Result edit(Long id)
+	{
+		Form<Asset> assetForm = formFactory.form(Asset.class).fill(Asset.find.byId(id));
+		return ok(views.html.edit.render(id, assetForm));
+	}
+
+	public Result update(Long id)
+	{
+		Form<Asset> assetForm = formFactory.form(Asset.class).bindFromRequest();
+		if (assetForm.hasErrors())
+		{
+			return badRequest(views.html.edit.render(id, assetForm));
+		}
+
+		Transaction transaction = Ebean.beginTransaction();
+		try
+		{
+			Asset savedAsset = Asset.find.byId(id);
+			if (savedAsset != null)
+			{
+				Asset updatedAsset = assetForm.get();
+
+				savedAsset.name = updatedAsset.name;
+				savedAsset.description = updatedAsset.description;
+				savedAsset.last_access_by = updatedAsset.last_access_by;
+				savedAsset.last_access_date = updatedAsset.last_access_date;
+
+				savedAsset.update();
+				transaction.commit();
+			}
+		}
+		finally
+		{
+			transaction.end();
+		}
+
+		return GO_HOME;
 	}
 }
